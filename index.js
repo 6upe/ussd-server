a8c8264f747190fe04a5a2623ba8ba6530fa9bdd
Bupe Katongo <107362236+6upe@users.noreply.github.com>
2023-09-09 12:56:22 -0700

    ussd working with ngrok

--- index.js
+++ index.js
@@ -13,16 +13,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', (req, res) => {
-    const clientData = req.body;
-    console.log(clientData);
-    res.send(clientData);
+    const clientData = req.body
+    // console.log(clientData.phoneNumber);
+    const text = clientData.text;
+    const phoneNumber = clientData.phoneNumber;
+
+    let response = '';
+
+    if (text == '') {
+        // This is the first request. Note how we start the response with CON
+        response = `CON What would you like to check
+        1. My account
+        2. My phone number`;
+    } else if ( text == '1') {
+        // Business logic for first level response
+        response = `CON Choose account information you want to view
+        1. Account number`;
+    } else if ( text == '2') {
+        // Business logic for first level response
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your phone number is ${phoneNumber}`;
+    } else if ( text == '1*1') {
+        // This is a second level response where the user selected 1 in the first instance
+        const accountNumber = 'ACC100101';
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your account number is ${accountNumber}`;
+    }
+
+    // Send the response back to the API
+    res.set('Content-Type', 'text/plain');
+    res.send(response);
 });
 
 app.post('/ussd', (req, res) => {
